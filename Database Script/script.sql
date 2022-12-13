1f78525673327f540977ce3802dc2cfd269fc337
Santanu Sarkar <SSA01@quantumscape.com>
2022-12-13 07:30:52 -0800

    additing database script folder

---
+++ Database Script/script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE sample;
+
+CREATE TABLE [sample].[dbo].[Employee1] (
+    Id int PRIMARY KEY,
+    FirstName varchar(50) NULL,
+    LastName varchar(50) NULL,
+    Salary decimal(18,2) NOT NULL,
+    DateOfBirth datetime NOT NULL,
+ );
+CREATE TABLE [sample].[dbo].[Dependent1]( 
+Id int PRIMARY KEY,
+FirstName varchar(50) NULL,
+LastName varchar(50) NULL,
+EmployeeId int NOT NULL FOREIGN KEY REFERENCES Employee1(Id),
+DateOfBirth datetime NOT NULL, 
+Relationship varchar(50) NOT NULL);
